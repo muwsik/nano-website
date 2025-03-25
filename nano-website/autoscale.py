fixed caching of auto-scaling results

nano-website/autoscale.py
@@ -3,10 +3,10 @@ import numpy as np
 import easyocr
 import re
 
-# Дополнительные функции, необходимые для обработки изображений
+import streamlit as st
 
-def findBorder(_fullImage, thr = 0.5):    
-    row_sum = np.sum(_fullImage, axis = 1, dtype = np.int64)
+def findBorder(c_fullImage, thr = 0.5):    
+    row_sum = np.sum(c_fullImage, axis = 1, dtype = np.int64)
 
     for i in range(len(row_sum) - 1):
         if np.abs(row_sum[i] - row_sum[i + 1]) >= row_sum[i] * thr:
@@ -14,13 +14,13 @@ def findBorder(_fullImage, thr = 0.5):
     
     return None
 
-def scaleLength(_fullImage, start_y):
-    __, width = _fullImage.shape
+def scaleLength(c_fullImage, start_y):
+    _, width = c_fullImage.shape
     first_white_index = None
     last_white_index = None
 
     for x in range(1, width):
-        if (_fullImage[start_y, x] >= 230 ) and (_fullImage[start_y, x-1] <= 25):
+        if (c_fullImage[start_y, x] >= 230 ) and (c_fullImage[start_y, x-1] <= 25):
             if first_white_index is None:
                 first_white_index = x
             last_white_index = x
@@ -31,23 +31,23 @@ def scaleLength(_fullImage, start_y):
     return None
 
 
-def findText(_footnoteImage):
+def findText(c_footnoteImage):
     reader = easyocr.Reader(["en"], gpu = False, verbose = False)
-    result = reader.readtext(_footnoteImage, detail = 0, blocklist = 'SOo')
+    result = reader.readtext(c_footnoteImage, detail = 0, blocklist = 'SOo')
     return ' '.join(result).lower()  
 
-def increase(_text):
+def increase(c_text):
     try:
-        matchesIncrease = re.findall(r'[x][0-9]*.?[0-9]+[k]', _text)[0]
+        matchesIncrease = re.findall(r'[x][0-9]*\.?[0-9]+[k]', c_text)[0]
         _increase = float(matchesIncrease[1:-1])
     except Exception:
         _increase = None
 
     return _increase
 
-def scale(_text):
+def scale(c_text):
     try:
-        matchesScale = re.findall(r"[0-9]*\.?[0-9]+[nup]m", _text)[0]
+        matchesScale = re.findall(r"[0-9]*\.?[0-9]+[nup]m", c_text)[0]
         if matchesScale[-2] == 'n':
             _scale = float(matchesScale[:-2])
         if matchesScale[-2] == 'u' or matchesScale[-2] == 'p':
@@ -57,12 +57,13 @@ def scale(_text):
 
     return _scale
 
-def estimateScale(_image):
-    lowerBound = findBorder(_image)
+@st.cache_data(show_spinner = False)
+def estimateScale(c_image):
+    lowerBound = findBorder(c_image)
     if (lowerBound is not None):      
-        text = findText(_image[lowerBound:, :])
+        text = findText(c_image[lowerBound:, :])
         scaleVal = scale(text)
-        scaleLengthVal = scaleLength(_image, lowerBound)
+        scaleLengthVal = scaleLength(c_image, lowerBound)
 
         if (scaleVal is not None) and (scaleLengthVal is not None):
             return scaleVal / scaleLengthVal

nano-website/nano_website.py
@@ -2,11 +2,10 @@ import streamlit as st
 
 import io, csv
 import numpy as np
-from skimage import morphology
-from skimage.filters import median
 from streamlit_image_comparison import image_comparison
 from PIL import Image, ImageDraw
-    
+import time    
+
 import style, autoscale, nanoStatistics
 import ExponentialApproximation as EA
 
@@ -29,8 +28,6 @@ def load_default_settings():
     st.session_state['param3'] = 0.8
     st.session_state['param-pre-1'] = 10
     
-    st.session_state['rerun'] = True
-
     st.session_state['detected'] = False
     st.session_state['BLOBs'] = None
     st.session_state['BLOBs_params'] = None
@@ -47,6 +44,8 @@ def load_default_settings():
     st.session_state['recalculation'] = True
 
     st.session_state['calcStatictic'] = False
+
+    st.session_state['timeDetection'] = None
 
 if 'imageUpload' not in st.session_state:
     load_default_settings()
@@ -146,7 +145,8 @@ with tabDetect:
                 
         # Detecting
         with st.spinner("Nanoparticles detection", show_time = True):
-            if pushProcc:                
+            if pushProcc:
+                timeStart = time.time()                
                 currentImage = np.copy(grayImage) 
          
                 lowerBound = autoscale.findBorder(grayImage)        
@@ -186,9 +186,13 @@ with tabDetect:
                 st.session_state['BLOBs'] = BLOBs
                 st.session_state['BLOBs_params'] = BLOBs_params
                 st.session_state['detected'] = True
-                
+                st.session_state['timeDetection'] = time.time() - timeStart
+        
         if st.session_state['detected']:
-            st.markdown(f"<p class = 'text'>Nanoparticles detected: <b>{st.session_state['BLOBs'].shape[0]}</b></p>", unsafe_allow_html=True)
+            st.markdown(f"""<p class = 'text'>
+                            Nanoparticles detected: <b>{st.session_state['BLOBs'].shape[0]}</b>
+                            ({st.session_state['timeDetection']:.2f} sec)
+                        </p>""", unsafe_allow_html=True)
 
 
         # Filtering settings and results